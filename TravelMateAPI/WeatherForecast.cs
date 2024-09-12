422aa1bb43ef8f006fd52ffbe6e565416dc9f227
hdangtran <tranhaidang202@gmail.com>
2024-09-12 13:09:56 +0700

    test cicd

--- TravelMateAPI/WeatherForecast.cs
+++ TravelMateAPI/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace TravelMateAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string? Test { get; set; }
     }
 }
