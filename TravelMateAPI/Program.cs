43236b94e9867bf82ab4d7dc01f5bedfcb2e4398
LuongTND <ducluongtran07@gmail.com>
2024-09-12 18:35:02 +0700

    add cors v3

--- TravelMateAPI/Program.cs
+++ TravelMateAPI/Program.cs
@@ -105,7 +105,6 @@ namespace TravelMateAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            \
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAllOrigins",
