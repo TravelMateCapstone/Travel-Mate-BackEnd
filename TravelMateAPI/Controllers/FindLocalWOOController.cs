c84053641fb3e735d9d3e9b05cfae6b6a7c0b40b
LuongTND <ducluongtran07@gmail.com>
2024-10-24 19:14:20 +0700

    update search local

--- TravelMateAPI/Controllers/FindLocalWOOController.cs
+++ TravelMateAPI/Controllers/FindLocalWOOController.cs
@@ -1,4 +1,5 @@
 ﻿using BussinessObjects;
+using BussinessObjects.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Repositories;
 using Repositories.Interface;
@@ -38,14 +39,50 @@ namespace TravelMateAPI.Controllers
         //}
 
         //Tìm có phân trang 
+        //[HttpGet("search-locals")]
+        //public async Task<IActionResult> SearchLocals([FromQuery] int travelerId, [FromQuery] int locationId, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        //{
+        //    // Giả sử bạn đã có một phương thức để lấy danh sách activityIds của traveler
+        //    List<int> travelerActivities = await GetTravelerActivitiesAsync(travelerId);
+
+        //    // Gọi FindLocalService để tìm người dùng local theo role "Local"
+        //    var matchingUsers = await _findLocalService.GetMatchingUsersAsync(locationId, travelerActivities, "Local", pageNumber, pageSize);
+
+        //    if (matchingUsers == null || matchingUsers.Count == 0)
+        //    {
+        //        return NotFound(new { message = "No matching local users found." });
+        //    }
+
+        //    return Ok(matchingUsers);
+        //}
+
         [HttpGet("search-locals")]
-        public async Task<IActionResult> SearchLocals([FromQuery] int travelerId, [FromQuery] int locationId, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        public async Task<IActionResult> SearchLocals([FromQuery] int travelerId, [FromQuery] int locationId, [FromQuery] int option = 0, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
         {
             // Giả sử bạn đã có một phương thức để lấy danh sách activityIds của traveler
             List<int> travelerActivities = await GetTravelerActivitiesAsync(travelerId);
 
-            // Gọi FindLocalService để tìm người dùng local theo role "Local"
-            var matchingUsers = await _findLocalService.GetMatchingUsersAsync(locationId, travelerActivities, "Local", pageNumber, pageSize);
+            // Tạo một biến để lưu kết quả tìm kiếm
+            List<ApplicationUser> matchingUsers = null;
+
+            // Sử dụng switch-case để thực hiện logic dựa trên giá trị của option
+            switch (option)
+            {
+                case 1:
+                    // Thực hiện logic khi option = 1
+                    matchingUsers = await _findLocalService.GetMatchingUsersAsync(locationId, travelerActivities, "Local", pageNumber, pageSize);
+                    break;
+
+                case 2:
+                    // Thực hiện logic khi option = 2
+                    matchingUsers = await _findLocalService.GetMatchingUsersAsync(locationId, travelerActivities, "Local", pageNumber, pageSize);
+                    break;
+
+                default:
+                    // Thực hiện logic mặc định (khi option không phải 1 hoặc 2)
+                    matchingUsers = await _findLocalService.GetMatchingUsersAsync(locationId, travelerActivities, "Local", pageNumber, pageSize);
+                    break;
+            }
 
             if (matchingUsers == null || matchingUsers.Count == 0)
             {
