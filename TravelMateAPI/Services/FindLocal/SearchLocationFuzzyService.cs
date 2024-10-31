e06e0750bc4e7e64da30b2e758c20a7fc9a0f717
LuongTND <ducluongtran07@gmail.com>
2024-10-31 23:49:52 +0700

    update Event

--- TravelMateAPI/Services/FindLocal/SearchLocationFuzzyService.cs
+++ TravelMateAPI/Services/FindLocal/SearchLocationFuzzyService.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessObjects;
 using FuzzySharp;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.Text;
 namespace TravelMateAPI.Services.FindLocal
 {
     public class SearchLocationFuzzyService
@@ -15,11 +17,12 @@ namespace TravelMateAPI.Services.FindLocal
         public async Task<List<LocationDTO>> SearchLocationsAsync(string query)
         {
             var locations = await _context.Locations.ToListAsync();
-            var normalizedQuery = query.Trim().ToLower();
-
+            //var normalizedQuery = query.Trim().ToLower();
+            // Chuẩn hóa query không dấu
+            var normalizedQuery = RemoveDiacritics(query.Trim().ToLower());
             // Tính toán độ tương tự giữa query và từng LocationName
             var results = locations
-                .Where(l => Fuzz.Ratio(normalizedQuery, l.LocationName.ToLower()) > 60) // Ngưỡng tương tự 70%
+                .Where(l => Fuzz.Ratio(normalizedQuery, RemoveDiacritics(l.LocationName.ToLower())) > 65) // Ngưỡng tương tự 70%
                 .Select(l => new LocationDTO
                 {
                     LocationId = l.LocationId,
@@ -28,6 +31,23 @@ namespace TravelMateAPI.Services.FindLocal
                 .ToList();
 
             return results;
+        }
+        // Hàm loại bỏ dấu
+        private string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
         }
     }
 }
