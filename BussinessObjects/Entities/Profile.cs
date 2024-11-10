6f4aed0b984d5150df328aed7565c134d0ca50f0
LuongTND <ducluongtran07@gmail.com>
2024-11-10 14:17:18 +0700

    add infomation to claim

--- BussinessObjects/Entities/Profile.cs
+++ BussinessObjects/Entities/Profile.cs
@@ -11,7 +11,6 @@ namespace BusinessObjects.Entities
 
         public int UserId { get; set; }
 
-        public string? FullName { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
 
