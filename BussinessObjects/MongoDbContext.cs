4210f74ceaa788182233fba59ffcd56da35f8507
Tran Hai Dang <tranhaidang202@gmail.com>
2024-11-20 16:22:17 +0700

    fix mongodbcontext

--- BussinessObjects/MongoDbContext.cs
+++ BussinessObjects/MongoDbContext.cs
@@ -9,15 +9,14 @@ namespace BusinessObjects
 
         public MongoDbContext(IConfiguration configuration)
         {
-            ////var connectionString = configuration.GetConnectionString("MongoDbConnection");
-            //var mongoClient = new MongoClient("mongodb+srv://hdangtran:Tranhaidang2@@travelmate.pxwv7.mongodb.net/?retryWrites=true&w=majority&appName=TravelMate");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            configuration = builder.Build();
+            var connectionString = configuration.GetConnectionString("MongoDbConnection");
 
-
-            const string connectionUri = "mongodb+srv://hdangtran:Tranhaidang@travelmate.pxwv7.mongodb.net/?retryWrites=true&w=majority&appName=TravelMate";
-            var settings = MongoClientSettings.FromConnectionString(connectionUri);
-            // Set the ServerApi field of the settings object to set the version of the Stable API on the client
+            var settings = MongoClientSettings.FromConnectionString(connectionString);
             settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-            // Create a new client and connect to the server
             var client = new MongoClient(settings);
 
             _database = client.GetDatabase("TravelMate");
