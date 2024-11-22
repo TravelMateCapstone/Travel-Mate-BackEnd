2aa384a2d2a02e17f7900de693d598abc79a74a5
Tran Hai Dang <tranhaidang202@gmail.com>
2024-11-22 22:00:22 +0700

    update

--- TravelMateAPI/Hubs/ChatHub.cs
+++ TravelMateAPI/Hubs/ChatHub.cs
@@ -1,5 +1,4 @@
-﻿using BussinessObjects;
-using BussinessObjects.Entities;
+﻿using BusinessObjects;
 using Microsoft.AspNetCore.SignalR;
 
 namespace TravelMateAPI.Hubs
@@ -10,24 +9,6 @@ namespace TravelMateAPI.Hubs
         public ChatHub(ApplicationDBContext context)
         {
             _context = context;
-        }
-
-        public async Task SendMessage(string userId, string message)
-        {
-            var senderId = Context.UserIdentifier;
-
-            var chatMessage = new Message
-            {
-                SenderId = int.Parse(senderId),
-                ReceiverId = int.Parse(userId),
-                Text = message,
-                CreatedTime = DateTime.UtcNow
-            };
-
-            _context.Messages.Add(chatMessage);
-            await _context.SaveChangesAsync();
-
-            await Clients.User(userId).SendAsync("ReceiveMessage", message);
         }
 
         //
