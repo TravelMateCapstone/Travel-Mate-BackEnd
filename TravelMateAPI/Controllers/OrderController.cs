9f77c0afe81c9356697b7760077902a400fc4530
Tran Hai Dang <tranhaidang202@gmail.com>
2024-12-06 00:30:18 +0700

    update tour

--- TravelMateAPI/Controllers/OrderController.cs
+++ TravelMateAPI/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BusinessObjects.Entities;
+using BusinessObjects.Utils.Request;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Net.payOS;
@@ -26,22 +27,29 @@ namespace TravelMateAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> Create([FromBody] PaymentDto payment)
         {
-            var domain = "http://127.0.0.1:5500";
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var domain = "https://travelmatefe.netlify.app/";
 
             var paymentLinkRequest = new PaymentData(
                 orderCode: int.Parse(DateTimeOffset.Now.ToString("ffffff")),
-                amount: 2000,
+                amount: payment.Amount,
                 //ten chuyen di
-                description: "Thanh toan don hang",
+                description: payment.TourName,
                  //items: [new("Mì tôm hảo hảo ly", 1, 2000)],
                  items: null,
-                returnUrl: domain + "/success.html",
-                cancelUrl: domain + "/cancel.html"
+                returnUrl: domain + "contract/ongoing",
+                cancelUrl: domain + "contract/cancel"
             );
             var response = await _payOS.createPaymentLink(paymentLinkRequest);
 
+            //update payment status of traveler if success
             return Redirect(response.checkoutUrl);
         }
 
